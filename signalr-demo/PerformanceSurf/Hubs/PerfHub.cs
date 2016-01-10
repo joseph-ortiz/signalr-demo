d7dd7a4882cc47e24af0ac3a719f199cb00e90fb
joseph ortiz <joseph.ortiz@careerbuilder.com>
2016-01-10 18:25:14 -0500

    added base signalr

---
+++ signalr-demo/PerformanceSurf/Hubs/PerfHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace PerformanceSurf.Hubs
+{
+    public class PerfHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello("Hi!");
+        }
+    }
+}
